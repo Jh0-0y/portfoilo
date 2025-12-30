dae32d76392cb78b4ed87e8f113c62d72c98a2f6
jhy <idpc8090@gmail.com>
2025-12-30 16:17:36 +0900

    [refactor]

--- src/router/AdminRouter.tsx
+++ src/router/AdminRouter.tsx
@@ -1,11 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
-
 export default function AdminRouter() {
   return (
-    <Routes>
-      {/* <Route element={<AdminLayout />}>
-        <Route index element={<AdminDashboardPage />} />
-      </Route> */}
-    </Routes>
+<>
+</>
   )
 }
